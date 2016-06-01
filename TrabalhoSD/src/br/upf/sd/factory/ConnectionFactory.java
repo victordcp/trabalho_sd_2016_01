9279e228a6a0a22daa26731cf8f9406c3e7c598e
Fabricio Bedin <fabricioobedin@gmail.com>
2016-06-01 19:34:29 -0300

    alteração dados de acesso ao banco

--- TrabalhoSD/src/br/upf/sd/factory/ConnectionFactory.java
+++ TrabalhoSD/src/br/upf/sd/factory/ConnectionFactory.java
@@ -8,9 +8,9 @@ import java.sql.ResultSet;
 public class ConnectionFactory {
     // Variáveis com o caminho do banco de dados.
     private static final String DRIVER = "org.postgresql.Driver";
-    private static final String URL = "jdbc:postgresql://localhost:5432/nome";
+    private static final String URL = "jdbc:postgresql:ec2-52-67-44-247.sa-east-1.compute.amazonaws.com:5432/trabalhosd";
     private static final String USUARIO = "postgres";
-    private static final String SENHA = "masterkey";
+    private static final String SENHA = "";
 
     // metodo para criar a conexao com o banco
     public Connection abrirConexao() {
